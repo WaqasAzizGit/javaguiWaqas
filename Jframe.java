90a8170a78bb9c5edc477b1f40fcd8401af58505
Waqas <WaqasAziz2553@gmail.com>
2021-07-31 01:15:28 +0500

    SKIPPED

--- Jframe.java
+++ Jframe.java
@@ -1,4 +1,12 @@
-
+import javax.swing.*;
+import java.awt.*;
 public class Jframe {
+	public static void main(String args[])
+	{
+		JFrame f = new JFrame();
+		f.setTitle("first gui");
+		f.setSize(500,500);
+		f.setVisible(true);
+	}
 
 }
